3b19c066c49f62408f533ebfe8b44897b73ac324
Teguh Iqbal <teguh.iqbal782@gmail.com>
2024-09-27 00:20:36 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
     <div className="bg-black text-white">
       <header className="relative h-screen bg-[url('../../public/images/hero-bg.png')] bg-cover bg-center bg-no-repeat">
         <Navbar />
-        <div className="px-16 py-16 flex flex-col md:block">
-          <h1 className="font-trapp text-3xl md:text-8xl font-bold">
+        <div className="px-16 py-16">
+          <h1 className="font-trapp text-8xl font-bold">
             Manage your{" "}
-            <span className="font-trapp text-yellow-500 text-3xl md:text-8xl font-bold">
+            <span className="font-trapp text-yellow-500 text-8xl font-bold">
               money
             </span>
             <br />
@@ -41,17 +41,18 @@ export default function Home() {
             href="/city"
           >
             <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[#FFFFFF] rounded-[8px] transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
-            <span className="relative w-full md:w-[auto] text-center inline-block border-2 border-current px-7 py-3 text-sm font-bold uppercase tracking-widest rounded-[8px] bg-[#FFC654] transition-shadow group-focus:shadow-[#79995A]">
+            <span className="relative inline-block border-2 border-current px-7 py-3 text-sm font-bold uppercase tracking-widest rounded-[8px] bg-[#FFC654] transition-shadow group-focus:shadow-[#79995A]">
               <p className="font-trapp">Bangun Kotamu</p>
             </span>
           </Link>
         </div>
       </header>
 
-      <section className="md:h-screen grid md:grid-rows-2 md:my-0 my-10">
-        <div className="flex md:justify-end">
-          <div className="md:w-1/2 w-full content-center grid justify-items-center">
-            <h2 className="font-trapp md:text-5xl text-3xl font-bold">
+      <section className="h-screen grid grid-rows-2">
+        <div className="grid grid-cols-2">
+          <div className="col-1"></div>
+          <div className="col-2 content-center grid justify-items-center">
+            <h2 className="font-trapp text-5xl font-bold">
               Kembangkan Kotamu
               <br />
               Dan Raih Tujuan Untuk
@@ -62,7 +63,7 @@ export default function Home() {
             </h2>
           </div>
         </div>
-        <div className="md:grid grid-cols-1 gap-4 md:p-0 p-5">
+        <div className="grid grid-cols-1 gap-4">
           <div className="col-1">
             <Image
               loader={imageLoader}
@@ -70,9 +71,9 @@ export default function Home() {
               alt="Drone"
               width={500}
               height={500}
-              className="md:flex md:inline-flex md:me-20 md:mb-0 mb-5"
+              className="flex inline-flex me-20"
             />
-            <p className="font-poppins md:text-base font-light md:flex md:inline-flex text-black-rgba">
+            <p className="font-poppins text-base font-light flex inline-flex text-black-rgba">
               Rancang strategi keuangan yang baik sambil menikmati <br />{" "}
               pengalaman bermain yang seru. Membagi antara <br /> berbagai
               kebutuhan yang kamu butuhkan dan <br /> kebutuhan yang kamu
@@ -84,11 +85,11 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-screen mt-16 md:mt-0 flex items-center justify-center">
-        <div className="row grid md:grid-cols-3 md:gap-x-56 gap-x-5 items-center justify-center">
-          <div className="col-1 md:px-32 px-5">
-            <h3 className="font-trapp md:text-5xl text-3xl font-bold tracking-wide mb-12">
-              Tujuan City Of Money.
+      <section className="h-screen flex items-center justify-center">
+        <div className="row grid grid-cols-3 gap-x-56 flex items-center justify-center">
+          <div className="col-1 flex flex-col pl-20">
+            <h3 className="font-trapp text-5xl font-bold tracking-wide mb-12">
+              Tujuan <br /> City Of <br /> Money.
             </h3>
             <p className="font-poppins text-base font-light text-black-rgba">
               Membangun kota mu sendiri sebagai bagian dari program tabunganmu
@@ -104,10 +105,9 @@ export default function Home() {
               alt="Menara Bank"
               width={300}
               height={300}
-              className="max-sm:w-1/2 md:mx-0 mx-auto md:my-0 my-5"
             />
           </div>
-          <div className="col-3 grid md:grid-rows-3 grid-rows-2 md:p-0 p-5 md:gap-y-24 gap-10 justify-center">
+          <div className="col-3 grid grid-rows-3 gap-y-24">
             <div className="row-1">
               <Image
                 loader={imageLoader}
@@ -150,7 +150,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-screen flex md:flex-row flex-col gap-1.5 items-center justify-center md:mt-28 mt-10">
+      <section className="h-screen flex gap-1.5 items-center justify-center mt-28">
         <div
           className="card w-[390px] h-[443px] bg-cover bg-center flex items-center justify-center content-center"
           style={{ backgroundImage: "url('/images/card1.png')" }}
@@ -210,7 +210,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-screen grid md:grid-cols-2 md:my-0 my-10 gap-x-20 justify-items-center">
+      <section className="h-screen grid grid-cols-2 gap-x-20 justify-items-center">
         <div className="col-1">
           <Image
             loader={imageLoader}
@@ -220,8 +220,8 @@ export default function Home() {
             height={500}
           />
         </div>
-        <div className="col-2 items-center md:p-0 p-5">
-          <h3 className="font-trapp md:text-5xl text-3xl font-bold tracking-wide mb-14 text-white">
+        <div className="col-2 items-center">
+          <h3 className="font-trapp text-5xl font-bold tracking-wide mb-14 text-white">
             Investasikan{" "}
             <span className="text-black-rgba">
               sisa <br /> keuanganmu dengan <br /> pilihan yang
@@ -295,15 +295,15 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-[850px] bg-[#252121]">
+      <section className="h-[850px] bg-[#252121]">
         <div className="row grid grid-rows-1">
-          <h3 className="font-trapp md:text-5xl text-3xl font-bold tracking-wide mb-14 pt-28 text-center text-black-rgba">
+          <h3 className="font-trapp text-5xl font-bold tracking-wide mb-14 pt-28 text-center text-black-rgba">
             Dengan <span className="text-white">Element</span> <br /> Mudah
             Berinteraksi
           </h3>
         </div>
 
-        <div className="grid md:grid-cols-3">
+        <div className="grid grid-cols-3">
           <div className="col-1 grid grid-cols-2 gap-x-5">
             <div className="flex items-center justify-end">
               <div className="grid grid-rows-2 gap-y-5">
@@ -350,9 +350,9 @@ export default function Home() {
             />
           </div>
 
-          <div className="col-3 flex items-center justify-center  md:p-0 pt-16 pb-24">
+          <div className="col-3 flex items-center justify-center">
             <div>
-              <h4 className="font-trapp text-3xl md:mb-16 mb-5">Mobil</h4>
+              <h4 className="font-trapp text-3xl mb-16">Mobil</h4>
               <p className="font-poppins text-base font-light text-black-rgba">
                 Menggambarkan aliran transaksi <br />
                 keuangan, bergerak mengikuti <br />
@@ -365,8 +365,8 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-screen ">
-        <h3 className="font-trapp md:text-5xl text-3xl font-bold tracking-wide my-28 md:pt-40 text-black-rgba text-center">
+      <section className="h-screen ">
+        <h3 className="font-trapp text-5xl font-bold tracking-wide mb-14 pt-40 text-black-rgba text-center">
           Semua Dalam Satu <br /> <span className="text-white">Kota</span> Yang
           Sama
         </h3>
@@ -375,14 +375,14 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="row grid-rows-1 md:px-40 px-10 md:mt-0 mt-24">
-        <h3 className="font-trapp md:text-5xl text-3xl font-bold tracking-wide text-black-rgba">
+      <div className="row grid-rows-1 px-40">
+        <h3 className="font-trapp text-5xl font-bold tracking-wide text-black-rgba">
           Pertanyaan Yang <br /> <span className="text-white">Sering</span>{" "}
           Ditanyakan
         </h3>
       </div>
 
-      <section className="md:h-[500px] flex flex-col md:flex-row gap-1.5 md:items-start items-center justify-center md:mt-28 mt-10">
+      <section className="h-[500px] flex gap-1.5 items-start justify-center mt-28">
         <div className="card w-[390px] h-[443px] bg-[url('/images/card_kincir.png')] flex items-center justify-center content-center">
           <div>
             <h4 className="font-trapp text-3xl font-semibold tracking-wide mb-14">
@@ -432,25 +432,25 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="md:h-[450px] md:w-[auto] w-full grid grid-rows-1 justify-center items-center overflow-hidden">
+      <section className="h-[450px] grid grid-rows-1 justify-center items-center">
         <h1 className="font-trapp text-6xl font-semibold text-center">
           <TextRevealCardDemo />
         </h1>
       </section>
 
-      <section className="md:h-screen grid grid-rows-1 justify-center items-center">
-        <div className="md:bg-[url('/images/bg_biru.png')] bg-[#7DD2E3] md:rounded-none rounded-xl md:w-[1596px] md:h-[626px] md:grid md:grid-cols-2 flex flex-col-reverse md:p-0 p-10">
+      <section className="h-screen grid grid-rows-1 justify-center items-center">
+        <div className="bg-[url('/images/bg_biru.png')] w-[1596px] h-[626px] grid grid-cols-2">
           <div className="flex items-center justify-start">
-            <div className="md:ps-32">
-              <h4 className="font-trapp font-semibold md:text-5xl text-3xl text-[#2E2B2B] mb-5">
+            <div className="ps-32">
+              <h4 className="font-trapp font-semibold text-5xl text-[#2E2B2B] mb-5">
                 Menabung Lebih <br /> Menyenangkan <br /> dengan Simulasi.
               </h4>
-              <p className="font-poppins text-base font-light text-[#1B1A1ABF] mb-8 md:w-[64%] w-full">
-                Membangun kota mu sendiri sebagai bagian dari program{" "}
-                tabunganmu. Membangun kota mu sendiri sebagai bagian dari
-                program tabunganmu. Membangun kota mu sendiri sebagai bagian
-                dari program tabunganmu. Membangun kota mu sendiri sebagai
-                bagian dari.
+              <p className="font-poppins text-base font-light text-[#1B1A1ABF] mb-8">
+                Membangun kota mu sendiri sebagai bagian dari program <br />{" "}
+                tabunganmu. Membangun kota mu sendiri sebagai bagian <br /> dari
+                program tabunganmu. Membangun kota mu sendiri <br /> sebagai
+                bagian dari program tabunganmu. Membangun <br /> kota mu sendiri
+                sebagai bagian dari.
               </p>
 
               <Link
@@ -478,7 +478,7 @@ export default function Home() {
           <div className="flex items-center justify-end">
             <Image
               src="/images/gif/gedung_for_biru.gif"
-              className="md:mt-[-166px] md:pe-24 md:m-0 mb-10"
+              className="mt-[-166px] pe-24"
               alt="Gedung Animation"
               width={500}
               height={626}
