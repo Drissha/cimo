26f05741130078a1f989ab74f7fe6e9e20a938a3
Teguh Iqbal <teguh.iqbal782@gmail.com>
2024-09-26 14:57:26 +0700

    ignore eslint build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Mengabaikan ESLint saat build
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
